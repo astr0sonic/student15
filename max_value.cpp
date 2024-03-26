1f35b548df38a8959fd148636bf261f290a06f5d
denrules <dkolcow@gmail.com>
2024-03-26 16:02:51 +0300

    task1

--- max_value.cpp
+++ max_value.cpp
@@ -1,5 +1,18 @@
 #include "max_value.h"
+#include <vector>
 
 double calculateMaxValue(double* nums, int n) {
-    return 0.0;
+	double resmax = nums[0], resmin = nums[0];
+	for (int i = 1; i < n; i++) {
+		if (nums[i] != 0) {
+			auto res = { resmin + nums[i], resmin - nums[i], resmin * nums[i], resmin / nums[i], resmax + nums[i], resmax - nums[i], resmax * nums[i], resmax / nums[i] };
+			resmin = min(res);
+			resmax = max(res);
+		}
+		else {
+			auto res = { 0., resmin, resmax };
+			resmin = min(res);
+			resmax = max(res);
+		}
+	}
 }
