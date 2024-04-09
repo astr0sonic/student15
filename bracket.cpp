333c8fa0f3c29057be383037d5924cc9e9c66e70
denrules <dkolcow@gmail.com>
2024-04-09 10:56:35 +0300

    task5

--- bracket.cpp
+++ bracket.cpp
@@ -1,5 +1,67 @@
 #include "bracket.h"
 
 bool checkBrackets(const std::string& s) {
-    return true;
+	bool flag = true;;
+	for (int i = 0; i < s.size(); i++) {
+		char c = s[i];
+		string l = "";
+		switch (c) {
+		case '(':
+			i++;
+			c = s[i];
+			while (c != ')' || !flag) {
+				if (i >= s.size()) return false;
+				if (c == '(') flag = false;
+				if (c == ')' && !flag) flag = true;
+				l += c;
+				i++;
+				c = s[i];
+			}
+			if (!checkBrackets(l)) return false;
+			break;
+		case '[':
+			i++;
+			c = s[i];
+			while (c != ']' || !flag) {
+				if (i >= s.size()) return false;
+				if (c == '[') flag = false;
+				if (c == ']' && !flag) flag = true;
+				l += c;
+				i++;
+				c = s[i];
+			}
+			if (!checkBrackets(l)) return false;
+			break;
+		case '{':
+			i++;
+			c = s[i];
+			while (c != '}' || !flag) {
+				if (i >= s.size()) return false;
+				if (c == '{') flag = false;
+				if (c == '}' && !flag) flag = true;
+				l += c;
+				i++;
+				c = s[i];
+			}
+			if (!checkBrackets(l)) return false;
+			break;
+		case '<':
+			i++;
+			c = s[i];
+			while (c != '>' || !flag) {
+				if (i >= s.size()) return false;
+				if (c == '<') flag = false;
+				if (c == '>' && !flag) flag = true;
+				l += c;
+				i++;
+				c = s[i];
+			}
+			if (!checkBrackets(l)) return false;
+			break;
+		default:
+			if (c == ')' || c == ']' || c == '}' || c == '>') return false;
+			break;
+		}
+	}
+	return true;
 }
