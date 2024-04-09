d1cee5e5bd56789019a7287444c6e27ad2a4c316
denrules <dkolcow@gmail.com>
2024-04-09 11:26:15 +0300

    task6.1

--- rpn.cpp
+++ rpn.cpp
@@ -1,5 +1,39 @@
 #include "rpn.h"
+#include <string>
+#include <stack>
+using namespace std;
 
 double calculateRPN(const std::string& expr) {
-    return 1.0;
+	stack<double> st;
+	string l = "";
+	for (int i = 0; i < expr.size(); i++) {
+		char c = expr[i];
+		if (c <= '9' && c >= '0') l += c;
+		if (c == ' ' && !l.empty()) {
+			st.push(stod(l));
+			l = "";
+		}
+		if (c == '+' || c == '-' || c == '*' || c == '/') {
+
+			double a = st.top();
+			st.pop();
+			double b = st.top();
+			st.pop();
+			switch (c) {
+			case '+':
+				st.push(a + b);
+				break;
+			case '-':
+				st.push(b - a);
+				break;
+			case '*':
+				st.push(a * b);
+				break;
+			case '/':
+				st.push(b / a);
+				break;
+			}
+		}
+	}
+	return st.top();
 }
